07bd6f9474c2baeab1d0003bbd9d20348b7b68ac
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-13 15:53:26 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return <div>
+    <section><Navbar /></section>;
+  </div>
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div className='navbar'>
+          {/* Sidebar */}
+          <div className="wrapper">
+              
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
