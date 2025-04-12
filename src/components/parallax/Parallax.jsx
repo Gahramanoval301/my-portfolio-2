8fb73b9fee346ac63b0dc7256c1562d3367614c4
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-04-12 23:08:45 +0400

    correcting title of parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,7 +10,7 @@ const Parallax = ({ type }) => {
     const yBg= useTransform(scrollYProgress, [0,1], ["0%", "100%"])
     return (
         <div className='parallax' ref={ref} style={{ backgroundImage: type === "services" ? "linear-gradient(180deg, #464679, #0c0c1d, #000000)" : "linear-gradient(180deg, #111132,rgb(74, 74, 130), rgb(245, 239, 203))" }}>
-            <motion.h1 style={{y:yText}}> {type === "services" ? "What We Do?" : "What We Did?"}</motion.h1>
+            <motion.h1 style={{y:yText}}> {type === "services" ? "My Skills" : "My Projects"}</motion.h1>
             <motion.div className="mountains"></motion.div>
             <motion.div  className="planets"
                 style={{ y: yBg, backgroundImage: `url(${type === "services" ? "/planets.png" : "/sun.png"})`}}></motion.div>
