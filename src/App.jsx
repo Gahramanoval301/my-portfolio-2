5e5fb7263646a94f649c8ca24fd6e9f14918808f
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-04-12 23:07:37 +0400

    correcting structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
@@ -11,11 +12,11 @@ const App = () => {
       <Navbar />
       <Hero />
     </section>;
-    
-    <section id="Services "><Parallax type="services" /></section>;
-    <section>Services</section>;
+
     <section id="Portfolio"><Parallax type="portfolio" /></section>;
     <Portfolio />
+    <section id="Services "><Parallax type="services" /></section>;
+    <Services/>
     <section id="Contact">
       <Contact />
     </section>;
