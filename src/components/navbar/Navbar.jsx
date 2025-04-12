6fabb253fbe802e3dfde3aa8defb687419883327
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-04-12 20:16:59 +0400

    navbar section is added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
+import React from "react";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+import Sidebar from "../sidebar/Sidebar";
+import { FaLinkedin } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaNewspaper } from "react-icons/fa6";
 
+  // <a target="_blank" href="https://github.com/Gahramanoval301"><i
+  //                       class="fa-brands fa-github"></i></a>
+  //               <a target="_blank"
+  //                   href="https://www.linkedin.com/in/leman-gahramanova-0a3b6b2a5"><i
+  //                       class="fa-brands fa-linkedin-in"></i></a>
+  //               <a target="_blank"
+  //                   href="https://medium.com/@gahramanovalamann">
+  //                   <i class="fa-brands fa-medium"></i>
+  //               </a>
+  //               <a target="_blank"
+  //                   href="https://drive.google.com/file/d/14sHOEOxZlt89opVV2R1rKgx7YZBXzDh4/view?usp=sharing">
+  //                   <i class="fa-solid fa-newspaper">
+
+  //                   </i> </a>
+const socialMedias = [
+  {
+    id: 0,
+    link: "https://github.com/Gahramanoval301",
+    icon: <FaGithub />,
+  },
+  {
+    id: 1,
+    link: "https://www.linkedin.com/in/leman-gahramanova-0a3b6b2a5",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 2,
+    link: "https://medium.com/@gahramanovalamann",
+    icon: <FaMedium />,
+  },
+  {
+    id: 0,
+    link: "https://drive.google.com/file/d/14sHOEOxZlt89opVV2R1rKgx7YZBXzDh4/view?usp=sharing",
+    icon: <FaNewspaper />,
+  },
+  
+]
 const Navbar = () => {
   return (
-      <div className='navbar'>
-          {/* Sidebar */}
-          <div className="wrapper">
-              
-          </div>
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Gahramanova Leman
+        </motion.span>
+        <div className="social">
+          {
+            socialMedias.map((socialMedia) => (
+              <a style={{fontSize:20}}  key={socialMedia.id} href={socialMedia.link} target="_blank" rel="noreferrer">
+                {socialMedia.icon}
+              </a>
+            ))
+          }
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
