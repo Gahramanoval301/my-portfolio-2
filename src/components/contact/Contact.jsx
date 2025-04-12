dfd1cf433a8d8eef39180395fa3685848a89cfe6
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-04-12 23:07:52 +0400

    integrating backend api with contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss"
-import {easeIn, motion} from "framer-motion"
+import { easeIn, motion } from "framer-motion"
+import { sendMessage } from '../../api';
 
 const variants = {
-    initial:{
+    initial: {
         y: 500,
-        opacity:0
+        opacity: 0
     },
     animate: {
         y: 0,
         opacity: 1,
         transition: {
             duration: 0.5,
-            staggerChildren:0.1
+            staggerChildren: 0.1
         }
     }
 }
@@ -37,34 +38,55 @@ const formVariants = {
 };
 
 const Contact = () => {
-  return (
-      <motion.div className='contact' variants={variants} initial={"initial"} whileInView={"animate"}>
-          <motion.div className="textContainer" variants={variants} initial={"initial"} whileInView={"animate"}>
-              <motion.h1 variants={variants} >Let&apos;s work together</motion.h1>
-              <motion.div variants={variants} className="item">
-                  <h2>Mail</h2>
-                  <span>hello@gmail.com</span>
-              </motion.div>
-              <motion.div variants={variants} className="item">
-                  <h2>Address</h2>
-                  <span>hello@gmail.com</span>
-              </motion.div>
-              <motion.div variants={variants} className="item">
-                  <h2>Phone</h2>
-                  <span>+994513106096</span>
-              </motion.div>
-              
-          </motion.div>
-          <motion.div variants={formVariants} initial="initial" whileInView={"animate"}  className="formContainer">
-              <motion.form action="" variants={formVariants} initial="initial" whileInView={"animate"}>
-                  <motion.input type="text" required placeholder='Name' />
-                  <motion.input type="email" required placeholder='Email' />
-                  <motion.textarea rows={8} placeholder='Message' name="" id=""/>
-                  <motion.button>Submit</motion.button>
-              </motion.form>
-          </motion.div>
-    </motion.div>
-  )
+    const [data, setData] = useState({ title: "", email: "", message: "" });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const handleSubmit = async (e) => {
+        try {
+
+            setIsSubmitting(true);
+            e.preventDefault();
+            console.log(data);
+            const res = await sendMessage(data);
+            console.log(res);
+            if (res.success) {
+                alert("Thank you for your message!")
+                setData({ title: "", email: "", message: "" })
+            }
+        } catch {
+            alert("Please try again!")
+        }
+        finally {
+            setIsSubmitting(false);
+        }
+    }
+    return (
+        <motion.div className='contact' variants={variants} initial={"initial"} whileInView={"animate"}>
+            <motion.div className="textContainer" variants={variants} initial={"initial"} whileInView={"animate"}>
+                <motion.h1 variants={variants} >Let&apos;s work together</motion.h1>
+                <motion.div variants={variants} className="item">
+                    <h2>Mail</h2>
+                    <span>gahramanovalamann@gmail.com</span>
+                </motion.div>
+                <motion.div variants={variants} className="item">
+                    <h2>Address</h2>
+                    <span>Azerbaijan, Baku</span>
+                </motion.div>
+                <motion.div variants={variants} className="item">
+                    <h2>Phone</h2>
+                    <span>+994513106096</span>
+                </motion.div>
+
+            </motion.div>
+            <motion.div variants={formVariants} initial="initial" whileInView={"animate"} className="formContainer">
+                <motion.form variants={formVariants} initial="initial" whileInView={"animate"}>
+                    <motion.input onChange={(e) => setData((data) => ({ ...data, title: e.target.value }))} value={data.title} name='title' type="text" required placeholder='Name' />
+                    <motion.input onChange={(e) => setData((data) => ({ ...data, email: e.target.value }))} value={data.email} name='email' type="email" required placeholder='Email' />
+                    <motion.textarea onChange={(e) => setData((data) => ({ ...data, message: e.target.value }))} value={data.message} name='message' rows={8} placeholder='Message' id="" />
+                    <motion.button onClick={(e) => handleSubmit(e)}>{isSubmitting ? "Submitting..." : "Submit"}</motion.button>
+                </motion.form>
+            </motion.div>
+        </motion.div>
+    )
 }
 
 export default Contact
